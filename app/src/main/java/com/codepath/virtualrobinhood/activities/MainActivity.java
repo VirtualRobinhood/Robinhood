fd510bc833838ccc21793dd0a83105cacea4c3c5
gkurghin <gkurghinyan@linkedin.com>
2017-10-07 12:44:15 -0700

    fixed package names

--- app/src/main/java/com/codepath/virtualrobinhood/activities/MainActivity.java
+++ app/src/main/java/com/codepath/virtualrobinhood/activities/MainActivity.java
@@ -1,9 +1,9 @@
-package com.virtualrobinhood.activities;
+package com.codepath.virtualrobinhood.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.virtualrobinhood.R;
+import com.codepath.virtualrobinhood.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/test/java/com/codepath/virtualrobinhood/ExampleUnitTest.java
+++ app/src/test/java/com/codepath/virtualrobinhood/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.virtualrobinhood;
+package com.codepath.virtualrobinhood;
 
 import org.junit.Test;
 
