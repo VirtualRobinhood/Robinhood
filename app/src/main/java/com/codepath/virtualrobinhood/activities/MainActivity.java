f64f5b5b00ac8d05c9e59b4e91a0de0ef5b97f15
Sagar Mutha <sagar2305@gmail.com>
2017-10-07 20:46:01 -0700

    integrate fabric in the app

--- app/src/main/java/com/codepath/virtualrobinhood/activities/MainActivity.java
+++ app/src/main/java/com/codepath/virtualrobinhood/activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.codepath.virtualrobinhood.R;
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -25,6 +26,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener,
         View.OnClickListener {
@@ -39,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fabric.with(this, new Crashlytics());
 
         // [START config_signin]
         // Configure Google Sign In
